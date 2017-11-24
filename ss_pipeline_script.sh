a784fb9ee456dd067c9a50c258beb8f72808802f
Hye-Sun Jung <hyejung@deloitte.com.au>
2017-11-24 12:27:41 +1100

    initial commit

---
+++ ss_pipeline_script.sh
@@ -0,0 +1,8 @@
+curl -X POST \
+  http://docker.for.mac.localhost:18630/rest/v1/pipelines/import \
+  -H 'authorization: Basic YWRtaW46YWRtaW4=' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: multipart/form-data' \
+  -H 'postman-token: 57d503a7-011e-e83d-13e7-da0cd937e393' \
+  -H 'x-requested-by: sdc' \
+  -F 'file=@./ss_pipelines/exportedPipeline2.zip'
